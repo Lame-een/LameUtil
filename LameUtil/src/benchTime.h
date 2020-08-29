e088c0135c771bf149ee2e38fc69c69631d3b475
Lame-een <43285057+Lame-een@users.noreply.github.com>
2020-08-29 03:35:57 +0200

    Add: Macros to benchmarking class
    Added macros for benchmarking as well as a preprocessor definition to
    enable benchmarking.

--- LameUtil/src/benchTime.h
+++ LameUtil/src/benchTime.h
@@ -5,10 +5,21 @@
 /*
 RAII based timer class.
 
-Usage: 
+Usage:
 	Wrap the piece of code you want to measure into a scope.
 	Construct the class at the beginning of the scope
 	At the end of the scope the class gets destroyed and prints out the time elapse since construction to the standard output.
+
+	OR
+
+
+	Use the:
+		BENCHMARK_SCOPE(scopeName) - for benchmarking the scope
+		BENCHMARK_FUNCTION() - for benchmarking the function
+	macros with the following preprocessor definitions:
+		BENCHMARK_SEC - for seconds
+		BENCHMARK_MILLI - for milliseconds
+		BENCHMARK_MICRO - for microseconds
 
 BenchTimer()
 Default initializator, the timer will be in milliseconds.
@@ -17,6 +28,22 @@ BenchTimer(TimerType type)
 	type - enum which defines the precision of the timer, accepted values are: TimerType::MICRO, TimerType::MILLI, TimerType::SEC
 
 */
+
+#if BENCHMARK == 1
+#define BENCHMARK_SCOPE(scopeName) lameutil::BenchTimer timer##__LINE__(scopeName, lameutil::BenchTimer::TimerType::SEC)
+#elif BENCHMARK == 2
+#define BENCHMARK_SCOPE(scopeName) lameutil::BenchTimer timer##__LINE__(scopeName, lameutil::BenchTimer::TimerType::MILLI)
+#elif BENCHMARK == 3
+#define BENCHMARK_SCOPE(scopeName) lameutil::BenchTimer timer##__LINE__(scopeName, lameutil::BenchTimer::TimerType::MICRO)
+#else
+#define BENCHMARK_SCOPE(scopeName)
+#endif
+
+#if BENCHMARK
+#define BENCHMARK_FUNCTION() BENCHMARK_SCOPE(__FUNCSIG__)
+#else 
+#define BENCHMARK_FUNCTION()
+#endif
 
 namespace lameutil
 {
@@ -29,16 +56,23 @@ namespace lameutil
 		};
 	private:
 		std::chrono::time_point<std::chrono::high_resolution_clock> startTime;
-		TimerType timerType = TimerType::MILLI;
+
+		std::string name;
+		TimerType timerType;
 	public:
 
-		BenchTimer()
+		BenchTimer() : name{}, timerType{ TimerType::MILLI }
 		{
 			startTime = std::chrono::high_resolution_clock::now();
 		}
-		BenchTimer(TimerType type)
+
+		BenchTimer(std::string name) : name{ name }, timerType{ TimerType::MILLI }
 		{
-			timerType = type;
+			startTime = std::chrono::high_resolution_clock::now();
+		}
+
+		BenchTimer(std::string name, TimerType type) : name{ name }, timerType{ type }
+		{
 			startTime = std::chrono::high_resolution_clock::now();
 		}
 
@@ -46,11 +80,13 @@ namespace lameutil
 		{
 			std::chrono::time_point<std::chrono::high_resolution_clock> endTime = std::chrono::high_resolution_clock::now();
 
-			if(timerType == TimerType::MICRO)
+			std::cout << "Timer " << name << ": ";
+
+			if (timerType == TimerType::MICRO)
 			{
 				std::cout << std::chrono::duration_cast<std::chrono::microseconds>(endTime - startTime).count() << " us";
 			}
-			else if(timerType == TimerType::MILLI)
+			else if (timerType == TimerType::MILLI)
 			{
 				std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime).count() << " ms";
 			}
